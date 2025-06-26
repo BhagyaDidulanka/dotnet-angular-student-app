13ae9ba7ddb41bc3cb33ef80f04c80a2af805e37
BhagyaDidulanka <didulankabhagya@gmail.com>
2025-06-26 08:37:48 +0530

    create Dto class and AppDbContext

---
+++ WebApplication1/WebApplication1/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(sc => new { sc.StudentId, sc.CourseId });
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Dtos/StudentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Dtos
+{
+    public class StudentDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string HomeTown { get; set; }
+        public string RegistrationNo { get; set; }
+    }
+}
